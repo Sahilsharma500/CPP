4d9bd10db0e369b179851e868aa8125ec4213e8b
Sahilsharma500 <4sahilsharma1@gmail.com>
2024-10-12 22:09:16 +0530

    inorder

--- binarytrees/createroot.cpp
+++ binarytrees/createroot.cpp
@@ -56,7 +56,18 @@ void getLevelNode(node* root){
             }
         }
     }
-}
+};
+void inorder(node* root) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    inorder(root->left);
+    cout << root-> data << " ";
+    inorder(root->right);
+
+};
 
 int main(){
     node *root = NULL;
